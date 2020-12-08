b5d0b5930832b700e32ec2ae6d6e20f32437500d
Vikas Tiwari <vt9937251@gmail.com>
2020-12-08 13:57:55 +0530

    Update InventoryMain.java

--- src/main/java/InventoryMain.java
+++ src/main/java/InventoryMain.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.lang.*;
+import java.util.regex.*;
 
 /*
                1. add validation Regex and others
                2. Convert Code to Modular Form
                3. Create Generalize Exceptions
-               4. Add one more option o
-               f Generating Report --> This option Export all the data (that are
+               4. Add one more option of Generating Report --> This option Export all the data (that are
                    available in the map at that instance) in the excel sheet using Apache POI framework (Read This Framework)
                5. Gracefully Handle Exception and where ever possible throw it manually to give user error message
 
@@ -26,8 +26,8 @@ public class InventoryMain {
 
    static {
        displayMap.put(1,"Shoes");
-       displayMap.put(2,"Sunglasses");
-       displayMap.put(3,"Mobiles");
+       displayMap.put(2,"Jeans");
+       displayMap.put(3,"Shirt");
    }
 
     public static void main(String[] args) {
@@ -36,12 +36,13 @@ public class InventoryMain {
         Scanner sc= new Scanner(System.in);
 
         while(true){
-
+            System.out.println("**************************");
             System.out.println("Press 1 to Enter Data");
             System.out.println("Press 2 to View Data");
             System.out.println("Press 3 to Modify Data");
             System.out.println("Press 4 to Delete Data");
             System.out.println("Press 5 to Exit");
+            System.out.println("**************************");
             System.out.println("Enter your choice:");
             input = sc.nextInt();
             System.out.println("Input you Entered "+ input);
@@ -52,8 +53,8 @@ public class InventoryMain {
 
             else if(String.valueOf(input).equalsIgnoreCase("2")){
                 System.out.println("Press 1 to View Shoes ");
-                System.out.println("Press 2 to View Sunglasses ");
-                System.out.println("Press 3 to View Mobiles ");
+                System.out.println("Press 2 to View Jeans ");
+                System.out.println("Press 3 to View Shirt ");
                 Integer view = sc.nextInt();
                 displayInventory(displayMap.get(view));
             }
@@ -63,10 +64,11 @@ public class InventoryMain {
             }else if(String.valueOf(input).equalsIgnoreCase("4")){
                 System.out.println("Enter the product which you want to delete: ");
                 System.out.println("Press 1 to Remove Shoes ");
-                System.out.println("Press 2 to Remove Sunglasses ");
-                System.out.println("Press 3 to Remove Mobiles ");
+                System.out.println("Press 2 to Remove Jeans ");
+                System.out.println("Press 3 to Remove Shirt ");
                 Integer remove = sc.nextInt();
-
+                displayInventory(displayMap.get(remove));
+                removeProduct(displayMap.get(remove));
 
             }
 
@@ -78,7 +80,10 @@ public class InventoryMain {
         }
     }
 
-    private static void addProduct(Product product, String productName){
+  
+    //Function to add item 
+  
+    private static void addProduct(Product product, String productName){    
        try{
 
            List<Product> products = inventoryMap.get(productName);
@@ -98,13 +103,44 @@ public class InventoryMain {
        }
     }
 
-    private static void removeProduct(Product product, String productName){
+  
+    //Function to remove item
+  
+    private static void removeProduct(String product){                      
 
-       List<Product> products = inventoryMap.get(productName);
+       List<Product> products = inventoryMap.get(product);
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the brand name that you want to delete : ");
+       String brand_name=sc.next();
 
+        Product temp=null;
+
+        for(Product p:products)
+
+        {
+
+            if(p.getBrand().equalsIgnoreCase(brand_name)){
+
+                System.out.println(brand_name+" is Successfully deleted from the cart");
+
+                temp=p;
+
+                break;
+
+            }else{
+                System.out.println("Entered Item not found");
+                break;
+            }
+
+        }
+
+        if(temp!=null)
+            products.remove(temp);
 
     }
-
+    
+  
+  //Function to view items of selected product
     private static void displayInventory(String product){
 
         System.out.println("*****************************************************");
@@ -121,17 +157,32 @@ public class InventoryMain {
 
     }
 
+
+
+    /*    
+    //Function to view all the item present in the Inventory
+    private static void displayInventory(String product){
+       System.out.println("*****************************************************");
+        for (Map.Entry<String, List<Product>> entry : inventoryMap.entrySet()) {
+            System.out.println( entry.getKey());
+            System.out.println( entry.getValue());//Returns the list of values
+        }
+        System.out.println("*****************************************************");
+    }
+
+    */
+
     public static void addProductToInventory(){
 
        Scanner sc = new Scanner(System.in);
 
-       System.out.println("Enter the Type of Product : {Shoes :: Sunglasses :: Mobiles}");
+       System.out.println("Enter the Type of Product : {Shoes :: Jeans :: Shirt}");
 
        while(true){
 
            System.out.println("Press 1 to Enter Shoes");
-           System.out.println("Press 2 to Enter Sunglasses");
-           System.out.println("Press 3 to Enter Mobiles");
+           System.out.println("Press 2 to Enter Jeans");
+           System.out.println("Press 3 to Enter Shirt");
            System.out.println("Press 4 to Return to main menu");
 
            int input = sc.nextInt();
@@ -158,6 +209,7 @@ public class InventoryMain {
 
                     System.out.println("Enter Brand Name : ");
                     String brand = sc.next();
+
                     System.out.println("Enter the size");
                     int size = sc.nextInt();
 
@@ -165,35 +217,22 @@ public class InventoryMain {
                     int quantity = sc.nextInt();
 
                     Product product = new Product(brand, size, quantity);
-                    addProduct(product,"Sunglasses");
+                    addProduct(product,"Jeans");
 
             }else if (String.valueOf(input).equalsIgnoreCase("3")) {
 
                     System.out.println("Enter Brand Name : ");
                     String brand = sc.next();
 
-                    System.out.println("Enter the RAM size");
+                    System.out.println("Enter the size");
                     int size = sc.nextInt();
 
                     System.out.println("Enter the Quantity : ");
                     int quantity = sc.nextInt();
 
                     Product product = new Product(brand, size, quantity);
-                    addProduct(product,"Mobiles");
+                    addProduct(product,"Shirt");
             }
-       }
-   }
-
-   public static void removeProductFromInventory(){
-       System.out.println("Press 1 to Delete Shoes ");
-       System.out.println("Press 2 to Delete Sunglasses ");
-       System.out.println("Press 3 to Delete Mobiles ");
-       System.out.println("Press 4 to Return to main menu");
-       Scanner sc = new Scanner(System.in);
-       Integer input = sc.nextInt();
-
-       if (String.valueOf(input).equalsIgnoreCase("1")){
-
        }
    }
 }
